eb5214b7f377a6c64cf948e4e863fcc0632e5609
Cameron Christensen <cam@sci.utah.edu>
2015-07-08 18:08:08 -0500

    add ability to set random seed for parallel gen vecs

--- binary_convert/gen_vecs.cpp
+++ binary_convert/gen_vecs.cpp
@@ -5,9 +5,13 @@ int main(int argc,char**argv)
 {
   if (argc < 3)
   {
-    std::cout<<"Usage: gen_vecs <nvecs> <maxval>\n";
+    std::cout<<"Usage: gen_vecs <nvecs> <maxval> [random_seed]\n";
+    std::cout<<"Example: gen_vecs 512 2000 `od -vAn -N4 -tu4 < /dev/urandom`";
     return -1;
   }
+  if (argc > 3)
+    srand48(atol(argv[3]));
+
   unsigned nvecs=atol(argv[1]);
   double maxv=atof(argv[2]);
   double vec[3];
